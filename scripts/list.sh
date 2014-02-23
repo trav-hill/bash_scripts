ba0666432b776a8ae9e55298d20144f773eb5789
Travis Hill <travhill@Traviss-MacBook-Air.local>
2014-02-23 10:41:27 -0800

    added empty list handling

--- scripts/list.sh
+++ scripts/list.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 # 
 # createdby: Travis Hill
 #
 # Simple list display exercise
 i=1
 test_list="$@"
-for names in $test_list
-do
- echo "value $((i++)) :  $names"
-done
+if [ "$test_list" == "" ];
+  then
+    echo "No list entries rec'd. Exiting..."
+else
+  for names in $test_list
+    do
+      echo "value $((i++)) :  $names"
+    done
+fi
