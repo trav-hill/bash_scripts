0fc751c550807aa2edcb1c32835ca9041d8a3ffa
Travis Hill <travhill@Traviss-MacBook-Air.local>
2014-02-23 12:26:35 -0800

    updated to handle no files case

--- scripts/email_obfuscator.sh
+++ scripts/email_obfuscator.sh
@@ -4,8 +4,11 @@
 #
 #
 Pathing="$@"
-Filelist=`ls $Pathing*.txt`
-for file in $Filelist
-do
-  sed  -e 's/@[A-Za-z0-9]\{2,8\}\..\{2,3\}/@REDACTEDEMAIL\.ADDRESS/g' $file > $file.tmp && cp $file.tmp $file
-done
+if Filelist=`ls $Pathing*.txt 2>"$Pathing"redact-error.log`; then
+  for file in $Filelist
+  do
+    sed  -e 's/@[A-Za-z0-9]\{2,8\}\..\{2,3\}/@REDACTEDEMAIL\.ADDRESS/g' $file > $file.tmp && mv $file.tmp $file
+  done
+else    
+  echo "No files found!!";
+fi
